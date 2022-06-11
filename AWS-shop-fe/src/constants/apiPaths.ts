79152c7c9bede9ca5e732005d5f6bc8831bc7c13
Wit <battiwgood@gmail.com>
2022-06-11 12:47:21 +0300

    create branch

--- AWS-shop-fe/src/constants/apiPaths.ts
+++ AWS-shop-fe/src/constants/apiPaths.ts
@@ -1,10 +1,16 @@
-
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  // product:
+  //   "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
+  // order: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
+  // import: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
+  // bff: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
+  // cart: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
+
+  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
