16d29cd5710f238d5d132117cd340176c9016264
Wit <battiwgood@gmail.com>
2022-07-02 21:32:21 +0300

    fix: files apiPaths.ts PageProductImport.tsx CSVFileimport.tsx

--- AWS-shop-fe/src/constants/apiPaths.ts
+++ AWS-shop-fe/src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   // product:
   //   "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
   order: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
-  import: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
+  import: "https://sfc4iwmuik.execute-api.eu-west-1.amazonaws.com/dev/import",
+
   bff: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
   cart: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
 
