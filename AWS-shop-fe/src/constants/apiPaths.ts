c832aa2b5f1742034eae690ca560853d1d1da52a
Wit <battiwgood@gmail.com>
2022-08-07 17:46:12 +0300

    init:task9

--- AWS-shop-fe/src/constants/apiPaths.ts
+++ AWS-shop-fe/src/constants/apiPaths.ts
@@ -5,13 +5,6 @@ const API_PATHS = {
   order: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
   // import: "https://645gkyvbe9.execute-api.eu-west-1.amazonaws.com/dev/import", // ussing WP
   import: "https://9lyka8f6d9.execute-api.eu-west-1.amazonaws.com/dev/import", // without WP
-
-  // bff: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
-  // cart: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev/products",
-
-  // product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  // order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  // import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://07yjzeof27.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "http://battiw-cart-api-develop1.eu-west-1.elasticbeanstalk.com/api",
 };
